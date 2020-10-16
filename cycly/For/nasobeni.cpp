2935824edda0200569c7071d6e1e655d75c9d485
Gryfell <Gryfell15@seznam.cz>
2020-10-16 21:47:33 +0200

    1.0

--- cycly/For/nasobeni.cpp
+++ cycly/For/nasobeni.cpp
@@ -4,7 +4,19 @@ using namespace std;
 
 int main() {
 	for (int i = 1; i <= 10; i++)
-		cout << i << ' ' ;
+		cout << i << ' ';
+	cout << endl; 
+	for (int i = 1; i <= 10; i++)
+		cout << i*2 << ' ';
+	cout << endl;
+	for (int i = 1; i <= 10; i++)
+		cout << i*3 << ' ';
+	cout << endl;
+	for (int i = 1; i <= 10; i++)
+		cout << i*4 << ' ';
+	cout << endl;
+	for (int i = 1; i <= 10; i++)
+		cout << i*5 << ' ';
 	cout << endl;
 		
 

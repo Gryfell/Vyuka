50cba0b975321d9d1ad8b11b3f1415a15c908495
Gryfell <Gryfell15@seznam.cz>
2020-10-16 22:41:57 +0200

    1,0

---
+++ cycly/For/ryba.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int i;
+	cout << "kolik si date ryb k veceri?" << endl;
+			cin >> i;
+			for (i;i>=1 ; i--)
+				cout << "<* )))-<" << endl;
+
+
+
+}
